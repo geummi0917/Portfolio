3a56ac5618f0509aa1cd0c47ccdbf8b78ac1f601
geummi <geummi0917@gmail.com>
2021-12-16 01:06:32 +0900

    3차 업데이트

---
+++ js/main.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+
+    // 앵커 이동 부드럽게
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+    
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+    
+    var controller = new ScrollMagic.Controller();
+    var sectionProfile = document.querySelector('.section--profile')
+    
+    // 홈 타이틀 클릭하면 전체 페이지 나타내기
+    $('.home--title svg').click(function () {
+        var section = document.querySelectorAll('.body__container > .section');
+        for (let i = 0; i < section.length; i++) {
+            section[i].style.display = "block";
+        }
+        
+        $('.section--home .home--img svg').css({
+            "position": "relative"
+        })
+        
+        
+        // 스크롤 시 헤더 나타내기
+        var scene = new ScrollMagic.Scene({
+                triggerElement: sectionProfile,
+                triggerHook: 0.5,
+            })
+            .setClassToggle("header", "visible")
+            .addTo(controller);
+
+
+        // 홈 타이틀 클릭하면 프로필로 이동
+        document.querySelector('#profile').scrollIntoView({
+            behavior: "smooth"
+        });
+        
+    })
+
+    // 프로필 섹션에서 각 항목 누르면 나타나기
+    $('.tab').click(function(e) {
+        var tabNum = document.querySelectorAll('.tab');
+        var boxNum = document.querySelectorAll('.box');
+        
+        for(let i=0; i<tabNum.length; i++) {
+            if(e.target.value === i) {
+                boxNum[i].style.display = "flex";
+                tabNum[i].classList.toggle("clicked", true);
+            } else {
+                boxNum[i].style.display = "none";
+                tabNum[i].classList.toggle("clicked", false);
+            }
+        }
+    })
+
+})
