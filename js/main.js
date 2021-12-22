b4fca87f1f1dfab6042139959323799dc790328c
geummi <geummi0917@gmail.com>
2021-12-23 01:50:09 +0900

    4차 전체 수정 업데이트

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ $(document).ready(function() {
     // 홈 타이틀 클릭하면 전체 페이지 나타내기
     $('.home--title svg').click(function () {
         var section = document.querySelectorAll('.body__container > .section');
+        var portfolio = document.querySelector('.section--portfolio');
         for (let i = 0; i < section.length; i++) {
             section[i].style.display = "block";
         }
@@ -31,8 +32,8 @@ $(document).ready(function() {
                 triggerElement: sectionProfile,
                 triggerHook: 0.5,
             })
-            .setClassToggle("header", "visible")
-            .addTo(controller);
+            .addTo(controller)
+            .setClassToggle("header", "visible");
 
 
         // 홈 타이틀 클릭하면 프로필로 이동
