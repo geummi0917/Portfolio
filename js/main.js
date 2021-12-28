2cb021188272d37b8b2c08da3374e21255920b30
geummi <geummi0917@gmail.com>
2021-12-29 02:30:57 +0900

    전체 수정

--- js/main.js
+++ js/main.js
@@ -11,20 +11,12 @@ $(document).ready(function() {
         });
     });
 
-    // $("#home--title").tooltip({
-    //     tooltipClass:'tooltipUI',
-    //     content: function(callback) {
-    //       callback($(this).prop('title'));
-    //     }
-    // })
-    
     var controller = new ScrollMagic.Controller();
     var sectionProfile = document.querySelector('.section--profile')
     
     // 홈 타이틀 클릭하면 전체 페이지 나타내기
     $('#home--title svg').click(function () {
         var section = document.querySelectorAll('.body__container > .section');
-        var portfolio = document.querySelector('.section--portfolio');
         for (let i = 0; i < section.length; i++) {
             section[i].style.display = "block";
         }
@@ -45,30 +37,5 @@ $(document).ready(function() {
         
     })
 
-    // // 프로필 섹션에서 각 항목 누르면 나타나기
-    // $('.tab').click(function(e) {
-    //     var tabNum = document.querySelectorAll('.tab');
-    //     var boxNum = document.querySelectorAll('.box');
-        
-    //     for(let i=0; i<tabNum.length; i++) {
-    //         if(e.target.value === i) {
-    //             boxNum[i].style.display = "flex";
-    //             tabNum[i].classList.toggle("clicked", true);
-    //         } else {
-    //             boxNum[i].style.display = "none";
-    //             tabNum[i].classList.toggle("clicked", false);
-    //         }
-    //     }
-    // })
-
-    const modalOpen = () => {
-        document.querySelector('modal').classList.remove('hidden');
-    }
-
-    const modalClose = () => {
-        document.querySelector('modal').classList.add('hidden');
-    }
-
-    document.querySelector('profile__box').addEventListener("click", "open");
 
 })
